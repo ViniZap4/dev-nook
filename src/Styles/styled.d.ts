30962d830b9209723611afea66417d810b65c898
vinizap4 <vinizap4@outlook.com>
2022-08-18 14:31:21 -0300

    solve contents, creating global styles, create theme

---
+++ src/Styles/styled.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string;
+
+    colors:{
+      primary: string;
+      secundary: string;
+  
+      background: string;
+      text:string;
+    },
+  }
+
+}
---
+++ src/Styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default{
+  title: 'dark',
+
+  colors:{
+    primary: '#333',
+    secundary: '#D63AF9',
+
+    background: '#222',
+    text:'#fff',
+  },
+}
