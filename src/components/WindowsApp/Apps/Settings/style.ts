5f87546fb48ca0622ce4c3c3cbcb146631efe447
vinizap4 <vinizap4@outlook.com>
2023-01-05 20:59:52 -0300

    improve perfomance, delete unused codes

---
+++ src/components/WindowsApp/Apps/Settings/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  
+  width: 100%;
+  height: 100%;
+
+  overflow-x: hidden;
+  overflow-y: auto;
+`
--- src/components/WindowsApp/WindowsAppContainer/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  top:0px;
-  left: 0px;
-`
